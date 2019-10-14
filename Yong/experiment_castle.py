f9b4af0d30978e9a5a2a9b9bd087fd0dc9794b9c
Yong-Zhuang <jazz14jazz@gmail.com>
2019-10-13 23:28:36 -0400

    experiments for castle

---
+++ Yong/experiment_castle.py
@@ -0,0 +1,87 @@
+import numpy as np
+import scipy
+import scipy.stats
+import sys
+import argparse
+import operator
+import pandas as pd
+#from __future__ import print_function
+import os
+import cPickle as pickle
+import math
+import tensorflow as tf
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from time import time
+from datetime import datetime
+from datetime import timedelta
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback, TensorBoard
+from keras.models import model_from_json
+from keras import backend as K
+from sklearn import metrics
+import get_samples as gs
+import castle
+#from keras.utils import multi_gpu_model
+
+
+np.random.seed(1337)  # for reproducibility
+nb_epoch = 1000  # number of epoch at training stage
+batch_size = 100  # batch size
+train_test = 3825
+
+def get_metrics(results, lead,y, pred):
+    out = pd.DataFrame(data={"y": y, "pred": pred})
+    print(out.T)
+    m_mae = metrics.mean_absolute_error(y, pred)
+    m_rmse = metrics.mean_squared_error(y, pred) ** 0.5
+    m_r2 = metrics.r2_score(y, pred)
+    results[lead]["MAE"] += m_mae
+    results[lead]["RMSE"] += m_rmse
+    results[lead]["R2"] += m_r2
+    return results
+
+if __name__== "__main__": 
+    parser = argparse.ArgumentParser(description='castle')    
+    parser.add_argument('-gpu', metavar='int', required=True, help='gpu index, using which GPU')
+    parser.add_argument('-look', metavar='int', required=True, help='# days look forward')
+    parser.add_argument('-lead', metavar='int', required=True, help='# days lead time')    
+    args = parser.parse_args() 
+    look = int(args.look)
+    lead = int(args.lead)
+    #set gpu for running experiment
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+    no_gpu = 1
+    
+    print ('start constructing samples.')    
+
+    esf, dsf, ob,fo, y_ob_sf,y_fo_sf = gs.get_samples(look = look, lead = lead, normalization = True)    
+    
+    train_esf, train_dsf,train_ob,train_fo,train_y_ob_sf,train_y_fo_sf = esf[:train_test], dsf[:train_test], ob[:train_test],fo[:train_test], y_ob_sf[:train_test],y_fo_sf[:train_test]
+    
+    test_esf, test_dsf,test_ob,test_fo,test_y_ob_sf,test_y_fo_sf = esf[train_test:], dsf[train_test:], ob[train_test:],fo[train_test:], y_ob_sf[train_test:],y_fo_sf[train_test:]
+    
+    print("training model...")
+    clf = castle.CASTLE(batch_size, nb_epoch,observed_conf=(look, input_observed.shape[-1]), forecasted_conf = (lead,input_forecasted.shape[-1]),latent_dim = 256,batchNormalization=False, regularization=False)
+    
+    clf.fit([train_esf, train_dsf,train_ob,train_fo], [train_y_ob_sf,train_y_fo_sf])
+    
+    
+    prediction = clf.predict(test_esf, test_ob,test_fo)
+    
+    results = {}
+    for lead in ["5 days","7 days","10 days","15 days"]:
+        results[lead] = {}
+        for metric in ["MAE", "RMSE", "R2"]:
+            results[model][metric] = 0
+    
+    
+    
+    results = get_metrics(results,"5 days", test_dsf[:,5], prediction[:,5])
+    results = get_metrics(results,"7 days", test_dsf[:,7], prediction[:,7])
+    results = get_metrics(results,"10 days", test_dsf[:,10], prediction[:,10])
+    results = get_metrics(results,"15 days", test_dsf[:,15], prediction[:,15])
+    df_results = pd.DataFrame(results)
+    df_results.to_csv("castle_result.csv.gz.", index=None, header=True, compression="gzip")  
--- Yong/get_samples.py
+++ Yong/get_samples.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 
-folder = "./data/"
+folder = "../data/"
 G_CSV_PATH = f"{folder}X_Ganges.csv"
 GY_CSV_PATH = f"{folder}Y_Ganges.csv"
 B_CSV_PATH = f"{folder}X_Brahmaputra.csv"
@@ -13,38 +14,55 @@ FP_CSV_PATH = f"{folder}total_forecast_precipitation_mean_spread_input.npy"
 LAT = np.arange(-19,45,1)
 LON = np.arange(60,188,1)
 
+def get_samples(look = 10, lead = 10, normalization = True):
+    Qx_Ganges = pd.read_csv(G_CSV_PATH)
+    Qx_Ganges.head()
 
-Qx_Ganges = pd.read_csv(G_CSV_PATH)
-Qx_Ganges.head()
+    Qy_Ganges = pd.read_csv(GY_CSV_PATH)
+    Qy_Ganges.head()
 
-Qy_Ganges = pd.read_csv(GY_CSV_PATH)
-Qy_Ganges.head()
+    Q = pd.concat([Qx_Ganges, Qy_Ganges], axis=1)
+    Q.head()
+    idx = []
+    for i in np.arange(1 - look, lead+1, 1):
+        idx.append("Q_" + str(i))
 
-Q = pd.concat([Qx_Ganges, Qy_Ganges], axis=1)
-Q.head()
+    target_Q = Q.loc[:,idx[1:]]#Q_-8...Q_10
 
-look = 10
-lead = 10
-idx = []
-for i in np.arange(1 - look, lead+1, 1):
-    idx.append("Q_" + str(i))
+    X_Q = Q.loc[:,idx[:-1]] #Q_-9...Q_9
+    if normalization:
+        normalizer = StandardScaler()
+        norm_num_data = normalizer.fit_transform(X_Q.values)
+        X_Q.loc[:,idx[:-1]] = norm_num_data
+        
+    mb_rf = pd.read_csv(rainfall_save_path)
+
+    rf = np.load(RF_CSV_PATH)#(4896, 20, 64, 128)
+    rf = rf.reshape(rf.shape[0], rf.shape[1],-1)
+    rf = rf[:,:,mb_rf["idx"].values]
+    if normalization:
+        for i in range(rf.shape[1]):
+            normalizer = StandardScaler()
+            norm_num_data = normalizer.fit_transform(rf[:,i,:])
+            rf[:,i,:] = norm_num_data    
     
-target_Q = Q.loc[:,idx[1:]]
-target_Q.head()
-
-X_Q = Q.loc[:,idx[:-1]]
-X_Q.shape, X_Q.head()
-
-mb_rf = pd.read_csv(rainfall_save_path)
-
-rf = np.load(RF_CSV_PATH)#(4896, 20, 64, 128)
-rf = rf.reshape(rf.shape[0], rf.shape[1],-1)
-rf = rf[:,:,mb_rf["idx"].values]
-fp = np.load(FP_CSV_PATH)#(4896, 64, 128, 15, 2)
-fp = np.moveaxis(fp, 3, 1)
-fp = fp[:, :, :, :, 0]#(4896, 15, 64, 128)
-fp = fp.reshape(fp.shape[0], fp.shape[1],-1)
-fp = fp[:,:,mb_rf["idx"].values]
-
-X_rf = np.concatenate((rf[:,-look:,:], fp[:,:lead-1,:]), axis=1)
-X_rf.shape
+    fp = np.load(FP_CSV_PATH)#(4896, 64, 128, 15, 2)
+    fp = np.moveaxis(fp, 3, 1)
+    fp = fp[:, :, :, :, 0]#(4896, 15, 64, 128)
+    fp = fp.reshape(fp.shape[0], fp.shape[1],-1)
+    fp = fp[:,:,mb_rf["idx"].values]
+    if normalization:
+        for i in range(fp.shape[1]):
+            normalizer = StandardScaler()
+            norm_num_data = normalizer.fit_transform(fp[:,i,:])
+            fp[:,i,:] = norm_num_data     
+    X_rf = np.concatenate((rf[:,-look:,:], fp[:,:lead-1,:]), axis=1)#(4896, 19, 689)
+    
+    input_encoder_streamflow, input_decoder_streamflow = X_Q.iloc[:look].values,X_Q.iloc[look:].values
+    input_observed,input_forecasted = X_rf[:,:look,:],X_rf[:,look:,:]
+    y_ob_sf,y_fo_sf = target_Q.iloc[:look].values,target_Q.iloc[look:].values
+    
+    if normalization:
+        print("MinMax normalization")
+        Pob, Px_mean, Px_spread = Normalization(Pob, Px_mean, Px_spread)
+    return input_encoder_streamflow, input_decoder_streamflow, input_observed,input_forecasted, y_ob_sf,y_fo_sf
